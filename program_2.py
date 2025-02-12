e42de889b5c29a073282d3810ed229944dadf68d
FriedCoconut64 <vansterapples@gmail.com>
2025-02-12 10:51:57 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,14 +1,25 @@
-# Program #2: Movie Tix
-# Write a program that has the user input various movie names and how many 
-# tickets are desired for each movie.  
-# At the end of the program it prints out the total number of tickets desired by the user.  
-# Use either a "for loop" or "while loop" to accomplish this.
-
 def main():
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
 
-
+    global more_movies
+    global Tickets
+    Tickets = 0
+    more_movies = True
+    while more_movies == True:
+        movie_name = (input("What movie would you like to see? "))
+        new_tickets = int(input("How many tickets would you like?"))
+        Tickets = Tickets + new_tickets
+        if new_tickets <= 0:
+            print("Error: Tickets cannot be negative or zero, please try again")
+        else:
+            prompt_again =  input("Would you like to see another movie? (type 'Y' for yes)")
+            if prompt_again == 'Y':
+                more_movies = True
+            else:
+                more_movies = False
+                print("Total number of tickets:", Tickets)
+                print("Thank you for your purchases")
+                break
 if __name__ == '__main__':
     main()
+
+    # Program #2, Donovan Thompson 2/12/25
